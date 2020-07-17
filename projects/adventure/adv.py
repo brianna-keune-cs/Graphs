3cd427695289d9e9bf8b3f9c6660b8ff35b93bb6
Brianna Keune <briannakeune@gmail.com>
2020-07-17 12:06:52 -0500

    so close to sprint mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,6 +4,8 @@ from world import World
 
 import random
 from ast import literal_eval
+from util import Queue
+from graph import Graph
 
 # Load world
 world = World()
@@ -11,13 +13,13 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -28,7 +30,114 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+reverse_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+map_history = Graph()
+all_rooms = world.rooms.keys()
+visited = []
 
+
+def move_player(direction):
+    '''
+    moves player to new room, and adds direction took to traversal path
+    '''
+    player.travel(direction)
+    traversal_path.append(direction)
+
+
+def track_player_move(exit):
+    '''
+    moves player, and records exit taken to which room
+    in map_history for prev_room, and the next_room
+    '''
+    prev_room = player.current_room.get_room_id()
+    move_player(exit)
+    current_room = player.current_room.get_room_id()
+    print(
+        f'\nprev_room: {prev_room}\ndirection_took: {exit}\ncurrent_room: {current_room}')
+
+    # adds history for movement, and it's inverse
+    map_history.add_edge(prev_room, exit, current_room)
+    print('new map entry: ', prev_room, map_history.visited_rooms[prev_room])
+    map_history.add_edge(current_room, reverse_directions[exit], prev_room)
+    track_unexplored_exits(current_room)
+    print('inverse map entry: ', current_room,
+          map_history.visited_rooms[current_room])
+
+
+def track_unexplored_exits(current_room):
+    for exit in player.current_room.get_exits():
+        if exit not in map_history.get_tracked_exits(current_room).keys():
+            map_history.add_edge(current_room, exit, "?")
+
+
+def traverse_map(direction):
+    prev_room = player.current_room.get_room_id()
+    track_player_move(direction)
+    current_room = player.current_room.get_room_id()
+    visited.append(prev_room)
+
+    if "?" not in map_history.get_tracked_exits(current_room).values():
+        # move back to previous room
+        print('\nbacktracking...\n')
+        back_track()
+
+    for exit, room in map_history.get_tracked_exits(current_room).items():
+        if room is '?':
+            traverse_map(exit)
+
+
+def back_track():
+    if len(visited) > 0:
+        prev_room = visited.pop()
+        print('popped: ', prev_room)
+        for direction, room in map_history.get_tracked_exits(player.current_room.id).items():
+            if room is prev_room:
+                move_player(direction)
+                for exit, room in map_history.get_tracked_exits(player.current_room.id).items():
+                    if room is '?':
+                        traverse_map(exit)
+
+
+def find_path():
+    current_room = player.current_room.get_room_id()
+    visited.append(current_room)
+    # q = Queue()
+
+    # initializes map_history for starting room
+    if current_room not in map_history.get_visited_rooms():
+        map_history.add_room(current_room)
+        track_unexplored_exits(current_room)
+
+    for exit, room in map_history.get_tracked_exits(current_room).items():
+        if room is '?':
+            # q.enqueue(exit)
+            print('exit: ', exit)
+            traverse_map(exit)
+
+    # while q.size() > 0:
+    #     direction = q.dequeue()
+    #     traverse_map(direction)
+
+
+find_path()
+print('VISITED: ', visited)
+# print('path: ', traversal_path)
+# print('path: ', visited)
+print('explored rooms: ', map_history.visited_rooms)
+
+'''
+so we have access to the
+    - room's id
+    - room's exits
+    - player can travel to one room at a time.
+    - my graph can take in making a map history
+
+input: starting room
+output: a list of directions to travel to each room in the map
+
+visited rooms are the graphs keys,
+unexplored directions are held by '?'
+'''
 
 
 # TRAVERSAL TEST
@@ -41,22 +150,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
---
+++ projects/adventure/graph.py
@@ -0,0 +1,238 @@
+"""
+Simple graph implementation
+"""
+from util import Stack, Queue  # These may come in handy
+
+
+class Graph:
+
+    """Represent a graph as a dictionary of visited_rooms mapping labels to edges."""
+
+    def __init__(self):
+        self.visited_rooms = {}
+
+    def add_room(self, room_id):
+        """
+        Add a vertex to the graph.
+        """
+        if room_id not in self.visited_rooms.keys():
+            self.visited_rooms[room_id] = {}
+            return
+        raise AssertionError(
+            f'room_id: {room_id} is already in graph.')
+        
+    def len_of_visited(self):
+        return len(self.visited_rooms.keys())
+    
+    def get_visited_rooms(self):
+        return self.visited_rooms.keys()
+
+    def add_edge(self, room_id, direction, next_room_id):
+        """
+        Add a directed edge to the graph.
+        """
+        if room_id not in self.visited_rooms.keys():
+            self.add_room(room_id)
+        if direction not in ["n", 's', 'e', 'w']:
+            return
+        if direction in self.visited_rooms.values():
+            raise AssertionError(
+                f'vertex: {direction} already has an edge to {room_id}.')
+            return
+        self.visited_rooms[room_id].__setitem__(direction, next_room_id)
+
+    def get_tracked_exits(self, room_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.visited_rooms[room_id]
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        q = Queue()
+        q.enqueue(starting_vertex)
+        visited = set()
+
+        while q.size() > 0:
+            current_node = q.dequeue()
+            if current_node not in visited:
+                visited.add(current_node)
+                print(current_node)
+                neighbours = self.get_neighbors(current_node)
+                for neighbour in neighbours:
+                    q.enqueue(neighbour)
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        s = Stack()
+        s.push(starting_vertex)
+        visited = set()
+
+        while s.size() > 0:
+            current_node = s.pop()
+            if current_node not in visited:
+                visited.add(current_node)
+                print(current_node)
+                neighbours = self.get_neighbors(current_node)
+                for neighbour in neighbours:
+                    s.push(neighbour)
+
+    def dft_recursive(self, starting_vertex, visited=set()):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+
+        This should be done using recursion.
+        """
+        if starting_vertex not in self.visited_rooms:
+            raise IndexError(
+                f'the starting vertext: {starting_vertex} is not a vertex.')
+
+        if starting_vertex not in visited:
+            visited.add(starting_vertex)
+            print(starting_vertex)
+            neighbours = self.get_neighbors(starting_vertex)
+            for neighbour in neighbours:
+                self.dft_recursive(neighbour, visited)
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        q = Queue()
+        q.enqueue([starting_vertex])
+        visited = set()
+
+        while q.size() > 0:
+            path = q.dequeue()
+            current_node = path[-1]
+            if current_node is destination_vertex:
+                return path
+            if current_node not in visited:
+                visited.add(current_node)
+                for neighbour in self.get_neighbors(current_node):
+                    path_copy = path.copy()
+                    path_copy.append(neighbour)
+                    q.enqueue(path_copy)
+
+    def dfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+        """
+        s = Stack()
+        s.push([starting_vertex])
+        visited = set()
+
+        while s.size() > 0:
+            path = s.pop()
+            current_node = path[-1]
+            if current_node is destination_vertex:
+                return path
+            if current_node not in visited:
+                visited.add(current_node)
+                for neighbour in self.get_neighbors(current_node):
+                    path_copy = path.copy()
+                    path_copy.append(neighbour)
+                    s.push(path_copy)
+
+    def dfs_recursive(self, vertex, destination_vertex, visited=set(), path=list()):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+
+        This should be done using recursion.
+        """
+        if len(path) is 0:
+            path.append(vertex)
+
+        visited.add(vertex)
+
+        if vertex is destination_vertex:
+            return path
+
+        for neighbour in self.get_neighbors(vertex):
+            if neighbour not in visited:
+                result = self.dfs_recursive(
+                    neighbour, destination_vertex, visited, path + [neighbour])
+                if result is not None:
+                    return result
+
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_room(1)
+    graph.add_room(2)
+    graph.add_room(3)
+    graph.add_room(4)
+    graph.add_room(5)
+    graph.add_room(6)
+    graph.add_room(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print(graph.visited_rooms)
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    graph.bft(1)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft(1)
+    graph.dft_recursive(1)
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6), '\n')
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    print(graph.dfs(1, 6), '\n')
+    print(graph.dfs_recursive(1, 6))
